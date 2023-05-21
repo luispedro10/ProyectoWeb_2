8b7bc065ad61d5aeea5c58b97d670b0e4abee1c7
luispedro10 <luis5pedro.2000@gmail.com>
2023-05-20 23:01:41 -0600

    zapatos y descripciones

---
+++ fsv-front-end/src/fake-data.js
@@ -0,0 +1,106 @@
+import shoesImage1 from './assets/shoes-1.jpg';
+import shoesImage2 from './assets/shoes-2.jpg';
+import shoesImage3 from './assets/shoes-3.jpg';
+import shoesImage4 from './assets/shoes-4.jpg';
+import shoesImage5 from './assets/shoes-5.jpg';
+import shoesImage6 from './assets/shoes-6.jpg';
+import shoesImage7 from './assets/shoes-7.jpg';
+import shoesImage8 from './assets/shoes-8.jpg';
+import shoesImage9 from './assets/shoes-9.jpg';
+import shoesImage10 from './assets/shoes-10.jpg';
+import shoesImage11 from './assets/shoes-11.jpg';
+import shoesImage12 from './assets/shoes-12.jpg';
+
+export const products = [{
+  id: '123',
+  name: 'Adidas Busenitz',
+  price: '799',
+  description: ' Zapatos de skate de alta calidad diseñados en colaboración con el legendario skater profesional Dennis Busenitz. Estos zapatos están diseñados para ofrecer una combinación perfecta de estilo, durabilidad y rendimiento en el skateboarding',
+  imageUrl: shoesImage1,
+  averageRating: '5.0',
+}, {
+  id: '234',
+  name: 'Jordans Azules',
+  price: '999',
+  description: 'Elegantes y versátiles zapatos deportivos que combinan estilo y funcionalidad a la perfección. Con su atractivo color azul y el emblemático diseño de los Jordans, estos zapatos te permiten destacar tanto en el terreno de juego como en la vida cotidiana.',
+  imageUrl: shoesImage2,
+  averageRating: '5.0',
+}, {
+  id: '345',
+  name: 'Jordans Rojos',
+  price: '999',
+  description: 'Son unos icónicos y vibrantes zapatos deportivos que combinan a la perfección estilo y rendimiento. Con su llamativo color rojo y su reconocible diseño de alta gama, estos zapatos te permiten destacar en cualquier ocasión.',
+  imageUrl: shoesImage3,
+  averageRating: '5.0',
+}, {
+  id: '456',
+  name: 'Fiesteros Belicosos',
+  price: '1099',
+  description: 'Los "Belicosos" son una innovadora y elegante opción de calzado diseñada para aquellos que desean destacar con un estilo audaz y sofisticado. Estos zapatos fusionan perfectamente la apariencia clásica de un calzado formal con un toque de rebeldía y fuerza.',
+  imageUrl: shoesImage4,
+  averageRating: '5.0',
+}, {
+  id: '567',
+  name: 'Tony Hawks',
+  price: '899',
+  description: 'El estilo de los 90 fue atrevido y extravagante. Por eso no es sorpresa que la adidas OZWEEGO también lo sea. Su silueta se inspira en esa época irreverente y en los modelos de running que triunfaban por aquel entonces. Esta versión actual luce una silueta esculpida que puedes combinar con cualquier prenda de tu armario. Los detalles metalizados y reflectantes le aportan un toque sofisticado. Adidas Ozweego Disponibles para Hombre. ',
+  imageUrl: shoesImage5,
+  averageRating: '5.0',
+}, {
+  id: '678',
+  name: 'Ozweegos',
+  price: '1099',
+  description: '"Ozweegos" son unos zapatos de vanguardia que destacan por su diseño único y vistoso. Con una estética futurista y un enfoque en la experimentación con materiales y formas, los "Ozweegos" se han convertido en un ícono de la moda contemporánea.',
+  imageUrl: shoesImage6,
+  averageRating: '5.0',
+}, {
+  id: '789',
+  name: 'Party Animals',
+  price: '1299',
+  description: 'Los "Party Animals" son una emocionante y colorida línea de zapatos diseñados para aquellos que desean destacar en cualquier evento o fiesta. Estos zapatos se caracterizan por su diseño divertido y extravagante, con detalles y adornos que representan diferentes animales y elementos festivos.',
+  imageUrl: shoesImage7,
+  averageRating: '5.0',
+}, {
+  id: '890',
+  name: 'Vans Azules',
+  price: '699',
+  description: 'Con el All Star Chuck 70 Converse reelabora un icónico con detalles y técnicas actualizados para celebrar el Chuck Taylor All Star original de los años 70. Se han realizado cambios menores en la suela de goma, que es ligeramente más alto, una puntera de goma más sustancial, un lienzo más pesado, costuras vintage, mientras que la entresuela ahora cuenta con una amortiguación avanzada para una experiencia cómoda. Converse All Star Chuck 70 Hi “Black” Disponible para Hombre',
+  imageUrl: shoesImage8,
+  averageRating: '5.0',
+}, {
+  id: '901',
+  name: 'Skechers Running',
+  price: '799',
+  description: 'Los "Skechers Running" son zapatos diseñados específicamente para correr y brindar un rendimiento óptimo durante tus sesiones de entrenamiento. Skechers, una reconocida marca de calzado deportivo, se ha destacado en el mercado por su enfoque en la comodidad y la tecnología innovadora',
+  imageUrl: shoesImage9,
+  averageRating: '5.0',
+},
+{
+  id: '901',
+  name: 'Vaqueros Woody',
+  price: '2099',
+  description: 'Los Woodies son una elegante opción de calzado diseñada para ocasiones especiales y eventos formales. Estos zapatos son el complemento perfecto para completar un atuendo sofisticado y profesional.',
+  imageUrl: shoesImage10,
+  averageRating: '5.0',
+},
+{
+  id: '789',
+  name: 'Botas Ingenieras',
+  price: '699',
+  description: 'Las "Botas Ingenieras" de nuestra tienda son una elección perfecta para aquellos que buscan un calzado resistente y con estilo. Estas botas se caracterizan por su diseño robusto y su construcción duradera, inspirada en las botas utilizadas por los ingenieros y trabajadores industriales.',
+  imageUrl: shoesImage11,
+  averageRating: '5.0',
+}, {
+  id: '890',
+  name: 'Basicones',
+  price: '799',
+  description: 'Los "Basicones" son unos zapatos clásicos y versátiles que se destacan por su estilo atemporal y su diseño sencillo pero elegante. Estos zapatos, disponibles en nuestra tienda, son una opción confiable para aquellos que buscan una combinación perfecta entre comodidad y sofisticación.',
+  imageUrl: shoesImage12,
+  averageRating: '5.0',
+}];
+
+export const cartItems = [
+  products[0],
+  products[2],
+  products[3],
+];
--- fsv-front-end/src/views/ProductDetailPage.vue
+++ fsv-front-end/src/views/ProductDetailPage.vue
@@ -3,14 +3,40 @@
   </template>
   
   <script>
-  export default {
-      name: 'ProductDetailPage',
-  };
-  </script>
-  
-  <style scoped>
-  h1 {
-      color: blue;
+export default {
+    name: 'ProductDetailPage',
+};
+</script>
+
+<style scoped>
+  #page-wrap {
+    margin-top: 16px;
+    padding: 16px;
+    max-width: 600px;
   }
-  </style>
+
+  #img-wrap {
+    text-align: center;
+  }
+
+  img {
+    width: 400px;
+  }
+
+  #product-details {
+    padding: 16px;
+    position: relative;
+  }
+
+  #add-to-cart {
+    width: 100%;
+  }
+
+  #price {
+    position: absolute;
+    top: 24px;
+    right: 16px;
+  }
+</style>
+
   
