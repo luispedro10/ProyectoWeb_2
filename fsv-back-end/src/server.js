2e0c6ad5036884530b8e3c93f43f64d46994fce4
luispedro10 <luis5pedro.2000@gmail.com>
2023-05-23 14:30:16 -0600

    delete

--- fsv-back-end/src/server.js
+++ fsv-back-end/src/server.js
@@ -127,7 +127,6 @@ const products = [{
     client.close();
   });
   
-  
   app.get('/api/products/:productId', async (req, res) => {
     const { productId } = req.params;
     const client = await MongoClient.connect(
@@ -139,28 +138,51 @@ const products = [{
     if (product) {
         res.status(200).json(product);
     } else {
-        res.status(404).json('Could not find the product!');
+        res.status(404).json('No se encontro el producto');
     }
     client.close();
 });
   
-  app.post('/api/users/:userId/cart', (req, res) => {
-    const { productId } = req.body;
-    const product = products.find(product => product.id === productId);
-    if (product) {
-      cartItems.push(product);
-      res.status(200).json(cartItems);
-    } else {
-      res.status(404).json('No se encontro el producto');
-    }
+app.post('/api/users/:userId/cart', async (req, res) => {
+  const { userId } = req.params;
+  const { productId } = req.body;
+  const client = await MongoClient.connect(
+    'mongodb://127.0.0.1:27017',
+    { useNewUrlParser: true, useUnifiedTopology: true },
+  );
+  const db = client.db('vue-db');
+  await db.collection('users').updateOne({ id: userId }, {
+    $addToSet: { cartItems: productId },
   });
-  
-  app.delete('/api/users/:userId/cart/:productId', (req, res) => {
-    const { productId } = req.params;
-    cartItems = cartItems.filter(product => product.id !== productId);
-    res.status(200).json(cartItems);
+  const user = await db.collection('users').findOne({ id: userId });
+  const cartItemIds = user.cartItems;
+  const cartItems = cartItemIds.map(id =>
+  products.find(product => product.id === id));
+  res.status(200).json(cartItems);
+  client.close();
+});
+
+app.delete('/api/users/:userId/cart/:productId', async (req, res) => {
+  const { userId, productId } = req.params;
+  const client = await MongoClient.connect(
+    'mongodb://127.0.0.1:27017',
+    { useNewUrlParser: true, useUnifiedTopology: true },
+  );
+  const db = client.db('vue-db');
+
+  await db.collection('users').updateOne({ id: userId }, {
+    $pull: { cartItems: productId },
   });
-  
-  app.listen(8000, () => {
-      console.log('Server is listening on port 8000');
-  });
+  const user = await db.collection('users').findOne({ id: userId });
+  const products = await db.collection('products').find({}).toArray();
+  const cartItemIds = user.cartItems;
+  const cartItems = cartItemIds.map(id =>
+    products.find(product => product.id === id));
+
+  res.status(200).json(cartItems);
+  client.close();
+});
+
+app.listen(8000, () => {
+    console.log('Server is listening on port 8000');
+});
