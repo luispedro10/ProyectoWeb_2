faa95fe2683c299a3a5e27e11636aef109b06b7d
luispedro10 <luis5pedro.2000@gmail.com>
2023-05-23 23:51:54 -0600

    finish

--- fsv-back-end/src/server.js
+++ fsv-back-end/src/server.js
@@ -2,18 +2,22 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { MongoClient } from 'mongodb';
 import path from 'path';
+import history from 'connect-history-api-fallback';
 
   const app = express();
   app.use(bodyParser.json());
-
   app.use('/images', express.static(path.join(__dirname, '../assets')));
-  
+  app.use(express.static(path.resolve(__dirname, '../dist'), { maxAge: '1y', etag: false }));
+  app.use(history());
+
   app.get('/api/products', async (req, res) => {
     const client = await MongoClient.connect(
-      'mongodb://127.0.0.1:27017',
+      process.env.MONGO_USER && process.env.MONGO_PASS
+      ? `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.wyreiug.mongodb.net/${process.env.MONGO_DBNAME}?retryWrites=true&w=majority`
+      : 'mongodb://127.0.0.1:27017',
       { useNewUrlParser: true, useUnifiedTopology: true },
     );
-    const db = client.db('vue-db');
+    const db = client.db(process.env.MONGO_DBNAME || 'vue-db');
     const products = await db.collection('products').find({}).toArray();
     res.status(200).json(products);
     client.close();
@@ -22,10 +26,12 @@ import path from 'path';
   app.get('/api/users/:userId/cart', async (req, res) => {
     const { userId } = req.params;
     const client = await MongoClient.connect(
-      'mongodb://127.0.0.1:27017',
+      process.env.MONGO_USER && process.env.MONGO_PASS
+      ? `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.wyreiug.mongodb.net/${process.env.MONGO_DBNAME}?retryWrites=true&w=majority`
+      : 'mongodb://127.0.0.1:27017',
       { useNewUrlParser: true, useUnifiedTopology: true },
     );
-    const db = client.db('vue-db');  
+      const db = client.db(process.env.MONGO_DBNAME || 'vue-db');  
     const user = await db.collection('users').findOne({ id: userId });
     if (!user) return res.status(404).json('No se encontro el usuario');
     const products = await db.collection('products').find({}).toArray();
@@ -39,10 +45,12 @@ import path from 'path';
   app.get('/api/products/:productId', async (req, res) => {
     const { productId } = req.params;
     const client = await MongoClient.connect(
-      'mongodb://127.0.0.1:27017',
+      process.env.MONGO_USER && process.env.MONGO_PASS
+      ? `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.wyreiug.mongodb.net/${process.env.MONGO_DBNAME}?retryWrites=true&w=majority`
+      : 'mongodb://127.0.0.1:27017',
       { useNewUrlParser: true, useUnifiedTopology: true },
     );
-    const db = client.db('vue-db');
+    const db = client.db(process.env.MONGO_DBNAME || 'vue-db');
     const product = await db.collection('products').findOne({ id: productId });
     if (product) {
         res.status(200).json(product);
@@ -56,10 +64,12 @@ app.post('/api/users/:userId/cart', async (req, res) => {
   const { userId } = req.params;
   const { productId } = req.body;
   const client = await MongoClient.connect(
-    'mongodb://127.0.0.1:27017',
+    process.env.MONGO_USER && process.env.MONGO_PASS
+      ? `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.wyreiug.mongodb.net/${process.env.MONGO_DBNAME}?retryWrites=true&w=majority`
+      : 'mongodb://127.0.0.1:27017',
     { useNewUrlParser: true, useUnifiedTopology: true },
   );
-  const db = client.db('vue-db');
+  const db = client.db(process.env.MONGO_DBNAME || 'vue-db');
   await db.collection('users').updateOne({ id: userId }, {
     $addToSet: { cartItems: productId },
   });
@@ -75,10 +85,12 @@ app.post('/api/users/:userId/cart', async (req, res) => {
 app.delete('/api/users/:userId/cart/:productId', async (req, res) => {
   const { userId, productId } = req.params;
   const client = await MongoClient.connect(
-    'mongodb://127.0.0.1:27017',
+    process.env.MONGO_USER && process.env.MONGO_PASS
+      ? `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.wyreiug.mongodb.net/${process.env.MONGO_DBNAME}?retryWrites=true&w=majority`
+      : 'mongodb://127.0.0.1:27017',
     { useNewUrlParser: true, useUnifiedTopology: true },
   );
-  const db = client.db('vue-db');
+  const db = client.db(process.env.MONGO_DBNAME || 'vue-db');
 
   await db.collection('users').updateOne({ id: userId }, {
     $pull: { cartItems: productId },
@@ -93,6 +105,10 @@ app.delete('/api/users/:userId/cart/:productId', async (req, res) => {
   client.close();
 });
 
-app.listen(8000, () => {
-    console.log('Server is listening on port 8000');
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is listening on port 8000');
 });
